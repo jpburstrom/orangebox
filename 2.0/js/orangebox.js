4d67c8d40b66e9ab75ea964ee6de5df13975a857
David Hamilton <nova706@gmail.com>
2011-05-18 21:55:06 -0600

    remove jqueryswipe

--- 2.0/js/orangebox.js
+++ 2.0/js/orangebox.js
@@ -368,10 +368,6 @@ else {
                             ob_caption.append('<p>'+obj.data('ob_data').ob_caption+'</p>');
                             $('#ob_content').append(ob_caption);
                         }
-                        $(document).swipe({
-                            swipeLeft: function() { oB.methods.navigate(-1); },
-                            swipeRight: function() { oB.methods.navigate(1); }
-                       });
                     //Check for Mins
                         if(wH < oB.settings.contentMinHeight) { wH = oB.settings.contentMinHeight + (oB.settings.contentBorderWidth*2); }
                         if(wW < oB.settings.contentMinWidth) { wW = oB.settings.contentMinWidth + (oB.settings.contentBorderWidth*2); }
@@ -717,59 +713,6 @@ else {
         };
     })(jQuery); 
 }
-(function($) {
-	$.fn.swipe = function(options) {
-		var defaults = {
-			threshold: {
-				x: 30,
-				y: 10
-			},
-			swipeLeft: function() { alert('swiped left') },
-			swipeRight: function() { alert('swiped right') }
-		};
-		var options = $.extend(defaults, options);
-		if (!this) return false;
-		return this.each(function() {
-			var me = $(this)
-			var originalCoord = { x: 0, y: 0 }
-			var finalCoord = { x: 0, y: 0 }
-			function touchStart(event) {
-				originalCoord.x = event.targetTouches[0].pageX
-				originalCoord.y = event.targetTouches[0].pageY
-			}
-			function touchMove(event) {
-			    event.preventDefault();
-				finalCoord.x = event.targetTouches[0].pageX
-				finalCoord.y = event.targetTouches[0].pageY
-			}
-			function touchEnd(event) {
-				var changeY = originalCoord.y - finalCoord.y
-				if(changeY < defaults.threshold.y && changeY > (defaults.threshold.y*-1)) {
-					changeX = originalCoord.x - finalCoord.x
-					
-					if(changeX > defaults.threshold.x) {
-						defaults.swipeLeft()
-					}
-					if(changeX < (defaults.threshold.x*-1)) {
-						defaults.swipeRight()
-					}
-				}
-			}
-			function touchStart(event) {
-				originalCoord.x = event.targetTouches[0].pageX
-				originalCoord.y = event.targetTouches[0].pageY
-				finalCoord.x = originalCoord.x
-				finalCoord.y = originalCoord.y
-			}
-			function touchCancel(event) { 
-			}
-			this.addEventListener("touchstart", touchStart, false);
-			this.addEventListener("touchmove", touchMove, false);
-			this.addEventListener("touchend", touchEnd, false);
-			this.addEventListener("touchcancel", touchCancel, false);
-		});
-	};
-})(jQuery);
 jQuery(document).ready(function($) {
     if (typeof orangebox_vars !== "undefined") { $('a[rel*=lightbox]').orangeBox(orangebox_vars); }
     else { $('a[rel*=lightbox]').orangeBox(); }
